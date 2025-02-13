3027a030e2e710731719a67a6a8db05422976729
Cauan <cauanpjcts@gmail.com>
2025-02-13 01:39:09 -0300

    Refactor + adding routes

--- src/compontents/ItemList.jsx
+++ src/compontents/ItemList.jsx
@@ -1,24 +1,29 @@
+import { Link } from "react-router-dom";
 import SingleItem from "./SingleItem";
 
 // eslint-disable-next-line react/prop-types
-const ItemList = ({ title, items }) => {
+const ItemList = ({ title, items, itemsArray, path, idPath }) => {
   //   console.log(items);
 
   return (
     <div className="item-list">
       <div className="item-list__header">
         <h2>{title} populares</h2>
-        <a className="item-list__link" href="">
+        <Link className="item-list__link" to={path}>
           Mostrar tudo
-        </a>
+        </Link>
       </div>
 
       <div className="item-list__container">
-        {Array(items)
-          .fill()
-          .map(() => (
-            // eslint-disable-next-line react/jsx-key
-            <SingleItem />
+        {itemsArray
+          // eslint-disable-next-line react/prop-types
+          .filter((currentValue, index) => index < items)
+          .map((currObj, index) => (
+            <SingleItem
+              {...currObj}
+              idPath={idPath}
+              key={`${title}-${index}`}
+            />
           ))}
       </div>
     </div>
--- src/compontents/Main.jsx
+++ src/compontents/Main.jsx
@@ -1,13 +1,26 @@
 import ItemList from "./ItemList";
-
+import { artistArray } from "../assets/database/artists";
+import { songsArray } from "../assets/database/songs";
 const Main = () => {
   return (
     <div className="main">
       {/* ItemList de Artistas */}
-      <ItemList title="Artistas" items={5} />
+      <ItemList
+        title="Artistas"
+        items={10}
+        itemsArray={artistArray}
+        path="/artists"
+        idPath="/artist"
+      />
 
       {/* ItemList de Músicas */}
-      <ItemList title="Músicas" items={10} />
+      <ItemList
+        title="Músicas"
+        items={20}
+        itemsArray={songsArray}
+        path="/songs"
+        idPath="/song"
+      />
     </div>
   );
 };
