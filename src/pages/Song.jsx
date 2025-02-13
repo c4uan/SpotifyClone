8ca192bf9f05275fb774e70ef0c9b5889be50f51
Cauan <cauanpjcts@gmail.com>
2025-02-13 03:24:03 -0300

    Song compontent ready

--- src/pages/Song.jsx
+++ src/pages/Song.jsx
@@ -1,5 +1,36 @@
+import { Link } from "react-router-dom";
+import Player from "../compontents/Player";
+
 const Song = () => {
-  return <div>Song</div>;
+  return (
+    <div className="song">
+      <div className="song__container">
+        <div className="song__image-container">
+          <img
+            src="https://i.scdn.co/image/ab67616d00001e022774b00531d558bc19e12a24"
+            alt="Imagem da Música X"
+          />
+        </div>
+      </div>
+
+      <div className="song__bar">
+        <Link to="/artist/1" className="song__artist-image">
+          <img
+            width={75}
+            height={75}
+            src="https://i.scdn.co/image/ab676161000051744dcd8a3bff84cd7703892cf4"
+            alt="Imagem do Artista Y"
+          />
+        </Link>
+
+        <Player />
+        <div>
+          <p className="song__name">Última Saudade - Ao Vivo</p>
+          <p>Henrique & Juliano</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Song;
