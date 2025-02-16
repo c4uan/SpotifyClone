6b29e8db8a51cc67a097003a67496a53f7240e84
Cauan <cauanpjcts@gmail.com>
2025-02-16 15:36:29 -0300

    ajustes para o deploy

--- back-end/api/connect.js
+++ back-end/api/connect.js
@@ -1,7 +1,7 @@
 import { MongoClient } from "mongodb";
 
 const URI =
-  "mongodb+srv://<db_username>:<db_password>@cluster0.miift.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+  "mongodb+srv://cauanmgn:6rEj2lQJKiKxqlKl@cluster0.miift.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 const client = new MongoClient(URI);
 
